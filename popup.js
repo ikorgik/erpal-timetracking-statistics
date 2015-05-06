000bbe00ec58e306a551753ae3c0f82fb7131937
Sergey Korzh <ikorgik@gmail.com>
2015-05-07 00:04:58 +0300

    Work version

--- popup.js
+++ popup.js
@@ -6,11 +6,10 @@
 
 
 (function ($) {
-
-  var name = 'admin';
-  var pass =  'test';
-  var url = "http://erpal.local/rest/projects/user/login.json";
+  var url = "http://erpal.brightsolutions.de/services/session/token";
+  //var url = "http://erpal.local/services/session/token";
   var tt_url = "http://erpal.local/rest/projects/timetracking/statistics.json";
+  //var tt_url = "http://erpal.brightsolutions.de/rest/projects/timetracking/statistics.json";
 
   $.ajax({
     url:"http://erpal.local/services/session/token",
@@ -21,7 +20,6 @@
     },
     success: function (token) {
       console.log(token);
-      // Call system connect with session token.
       $.ajax({
         url : tt_url,
         type : 'post',
@@ -30,7 +28,6 @@
         cache: false,
         beforeSend: function (request) {
           request.setRequestHeader("X-CSRF-Token", token);
-          //request.setRequestHeader("Cookie", cookie);
         },
         error : function(data) {
           //error code
@@ -43,52 +40,4 @@
       });
     }
   });
-//
-//  $.ajax({
-//    url: url,
-//    type: "POST",
-//    data: 'username=' + encodeURIComponent(name) + '&password=' + encodeURIComponent(pass),
-//    dataType : 'json',
-//    crossDomain: true,
-//    cache: false,
-//    error : function(data) {
-//
-//      //error code
-//      console.log(data);
-//    },
-//    success : function(data) {
-//      console.log(data);
-////      //success code
-////      var sessid = data.sessid;
-////      var session_name = data.session_name;
-////
-////      //var user = data.user;
-////      //console.log('session_name',session_name);
-////      //console.log('sessid',sessid);
-////
-////      $.cookie(session_name, sessid);
-////      var cookie = data.session_name + "=" + data.sessid;
-//
-//      $.ajax({
-//        url : tt_url,
-//        type : 'post',
-//        dataType : 'json',
-//        crossDomain: true,
-//        cache: false,
-//        beforeSend: function (request) {
-//          //request.setRequestHeader("X-CSRF-Token", data.token);
-//          //request.setRequestHeader("Cookie", cookie);
-//        },
-//        error : function(data) {
-//          //error code
-//          console.log('Error',data);
-//        },
-//        success : function(data) {
-//          //success code
-//          console.log('Cookie Accepted',data);
-//        }
-//      });
-//
-//    }
-//  });
 })(jQuery);
